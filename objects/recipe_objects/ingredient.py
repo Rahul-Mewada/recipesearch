ec1a7c6232ddb90ea2d3007a91befc2bce4aa0f7
Rahul Mewada <rahulmewada95@gmail.com>
2022-03-16 17:42:55 -0500

    Added ingredient class

--- objects/recipe_objects/ingredient.py
+++ objects/recipe_objects/ingredient.py
@@ -1,4 +1,9 @@
 
 class Ingredient:
-    def __init__(self, ingredient_str):
-        pass
+    def __init__(self, ingredient_dict):
+        self.name = None
+        self.unit = None
+        self.quantity = None
+        self.name = ingredient_dict["name"]
+        self.quantity = ingredient_dict["qty"]
+        self.unit = ingredient_dict["unit"]
