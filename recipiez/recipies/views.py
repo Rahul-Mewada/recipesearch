33dbaa7eafd07c5cdb3d8d8d877e82e0d9aa483d
Rahul Mewada <rahulmewada95@gmail.com>
2022-03-31 16:16:01 -0500

    Added basic ingredient and keyword search

--- recipiez/recipies/views.py
+++ recipiez/recipies/views.py
@@ -2,7 +2,7 @@ from typing import List
 from rest_framework import generics
 from rest_framework.views import APIView
 from .models import IngredientName, Recipe, VisitedUrl, Keyword, Ingredient
-from .serializers import IngredientNameSerializer, IngredientSerializer, KeywordRecipeSerializer, RecipeSerialzer, VisitedUrlSerializer,\
+from .serializers import IngredientNameSerializer, IngredientSerializer, KeywordRecipeSerializer, KeywordSerializer, RecipeSerialzer, VisitedUrlSerializer,\
      RecipeViewSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -70,3 +70,18 @@ def get_url_view(request, *args, **kwargs):
         if not data:
             return Response(data=[], status = 404)
     return Response(data, status = 200)
+
+@api_view(["GET"])
+def get_recipe_from_ingredient(request, *args, **kwargs):
+    qs_ingredient_name = IngredientName.objects.filter(name__contains=request.data['name'])
+    qs_ingredients = Ingredient.objects.filter(name__in = qs_ingredient_name)
+    qs_recipies = Recipe.objects.filter(ingredients__in = qs_ingredients).distinct()
+    serializer = RecipeViewSerializer(qs_recipies, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_recipe_from_keyword(request, *args, **kwargs):
+    qs_keywords = Keyword.objects.filter(keyword__contains = request.data['keyword']).distinct()
+    qs_recipies = Recipe.objects.filter(keywords__in = qs_keywords).distinct()
+    serializer = RecipeViewSerializer(qs_recipies, many=True)
+    return Response(serializer.data)
